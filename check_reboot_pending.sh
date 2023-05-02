8d1463a064f615c5956c5cbd90ba20a14766d1c3
John G <johngonzalez.fr@gmail.com>
2023-05-02 13:18:37 +0200

    Update check_reboot_pending.sh
    Added: Check if after an update, a new kernel was installed and if yes, give the information also that a reboot is pending to use the new kernel.

--- check_reboot_pending.sh
+++ check_reboot_pending.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 script="check_reboot_pending.sh"
-version="1.0.0"
+version="1.1.0"
 author="John Gonzalez"
 
 while getopts ":v" opt; do
@@ -19,14 +19,18 @@ done
 
 reboot_required_file="/var/run/reboot-required"
 reboot_pkgs_file="/var/run/reboot-required.pkgs"
+current_kernel=$(uname -r)
+latest_kernel=$(ls /boot | grep vmlinuz | sort -V | tail -n 1 | cut -d'-' -f2-)
 
 if [ -f "$reboot_required_file" ]; then
   pkgs=$(cat "$reboot_pkgs_file")
   reboot_pkgs_list=$(echo "$pkgs" | tr '\n' ',' | sed 's/,$//;s/,/, /g')
   echo "A reboot is pending - Packages asking for this reboot: $reboot_pkgs_list."
   exit 1 # WARNING
+elif [ "$current_kernel" != "$latest_kernel" ]; then
+  echo "A new kernel is installed. A reboot is pending to use the new kernel."
+  exit 1 # WARNING
 else
   echo "No reboot is required."
   exit 0 # OK
 fi
-
